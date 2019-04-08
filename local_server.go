e40d1e24bcb7319a819b3136fdd1eab1b6b7a5d5
zhouzhichao <zhouzhichao@bilibili.com>
2019-04-08 17:59:37 +0800

    fix map

--- local_server.go
+++ local_server.go
@@ -4,50 +4,51 @@ import "sync"
 
 // 通过channel实现全双工的Server&Client　方便测试consensus协议
 type LocalServer struct {
-	in chan *Packet
-	cm map[Client]Client
-	buf int
+	in           chan *Packet
+	cm           map[Client]Client
+	buf          int
 	connectMutex *sync.Mutex
 }
 
-func NewLocalServer(buf int)*LocalServer {
-	return &LocalServer{in:make(chan *Packet, buf), cm :make(map[Client]Client), buf:buf, connectMutex:&sync.Mutex{}}
+func NewLocalServer(buf int) *LocalServer {
+	return &LocalServer{in: make(chan *Packet, buf), cm: make(map[Client]Client), buf: buf, connectMutex: &sync.Mutex{}}
 }
 
 type LocalClient struct {
-	in chan *Packet
+	in  chan *Packet
 	out chan *Packet
 }
 
-func (s *LocalServer)Connect() Client {
+func (s *LocalServer) Connect() Client {
 	s.connectMutex.Lock()
 	defer s.connectMutex.Unlock()
 	in := make(chan *Packet, s.buf)
 	go func() {
-		for ;; {
-			p, ok  := <-in
+		for {
+			p, ok := <-in
 			if !ok {
 				break
 			}
+			s.connectMutex.Lock()
 			p.c = s.cm[p.c]
+			s.connectMutex.Unlock()
 			s.in <- p
 		}
 	}()
 
-	lc :=  &LocalClient{
-		in:in,
-		out:make(chan *Packet,s.buf),
+	lc := &LocalClient{
+		in:  in,
+		out: make(chan *Packet, s.buf),
 	}
 	s.cm[lc] = &LocalClient{
-		in:lc.out,
-		out:nil,
+		in:  lc.out,
+		out: nil,
 	}
-
 
 	return lc
 }
 
-func (s *LocalServer)Receive() <-chan *Packet {
+func (s *LocalServer) Receive() <-chan *Packet {
 	return s.in
 }
 
@@ -57,13 +58,13 @@ func (c *LocalClient) Send(p *Packet) error {
 	return nil
 }
 
-func (c *LocalClient) Receive()(p *Packet,err error) {
-	p = <- c.out
+func (c *LocalClient) Receive() (p *Packet, err error) {
+	p = <-c.out
 	p.c = c
 	return
 }
 
-func (c *LocalClient)Close() error {
+func (c *LocalClient) Close() error {
 	close(c.in)
 	close(c.out)
 	return nil
